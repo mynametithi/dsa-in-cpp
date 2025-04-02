f6e5f51ce1e85d76309192ebf1dc1a71b40313bd
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-04-02 10:05:20 +0530

    Add files via upload
    Signed-off-by: Tithi Saha <127840793+mynametithi@users.noreply.github.com>

--- 8.cpp
+++ 8.cpp
@@ -1,8 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-     
 
+int decToBinary(int decNum)
+{
+    int ans = 0;
+    int pow = 1;
+    while (decNum > 0)
+    {
+        int rem = decNum % 2;
+        decNum = decNum / 2;
+        ans = ans + (rem * pow);
+        pow = pow * 10;
+    }
+    return ans;
+}  
 
+int main()
+{
+    int decNum = 50;
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << decToBinary(i) << endl;
+    }
     return 0;
 }
