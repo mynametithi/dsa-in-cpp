0c53929330105ef742dde4139d85dce64b0b5bc0
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-04-04 18:28:28 +0530

    Add files via upload
    Signed-off-by: Tithi Saha <127840793+mynametithi@users.noreply.github.com>

---
+++ 12.cpp
@@ -0,0 +1,15 @@
+// call by value:
+#include <iostream>
+using namespace std;
+int changeX(int x)
+{
+    x = 2 * x;
+    cout << "x=" << x << endl;
+}
+int main()
+{
+    int x = 5;
+    changeX(x);
+    cout << "x=" << x << endl;
+    return 0;
+}
