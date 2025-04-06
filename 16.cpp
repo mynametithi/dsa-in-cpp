18eb169b50ac351d53d1bda5749bbe8104b8ee18
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-04-06 15:07:55 +0530

    Add files via upload
    Signed-off-by: Tithi Saha <127840793+mynametithi@users.noreply.github.com>

---
+++ 16.cpp
@@ -0,0 +1,15 @@
+// Bitwise Opereator:
+// Bitwise &(And)
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 4, b = 8;
+    cout << (a & b) << endl;
+    cout << (a | b) << endl;
+    cout << (a ^ b) << endl;
+    cout << (10 << 2) << endl;
+    cout << (8 >> 1) << endl;
+    return 0;
+}
