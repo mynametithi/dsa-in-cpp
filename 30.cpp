622ddcdff75de8a6c5ae2e4d2ef42fd03e78441f
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-05-14 08:08:15 +0530

    Add files via upload
    Signed-off-by: Tithi Saha <127840793+mynametithi@users.noreply.github.com>

---
+++ 30.cpp
@@ -0,0 +1,13 @@
+// vector:
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<char> vec = {'a', 'b', 'c', 'd', 'e'};
+    for (char i : vec)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
