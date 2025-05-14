e54de4ac9c7c2766b00fcd3d324821faef26abcd
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-05-14 08:07:35 +0530

    Add files via upload
    Signed-off-by: Tithi Saha <127840793+mynametithi@users.noreply.github.com>

---
+++ 28.cpp
@@ -0,0 +1,13 @@
+// vector:
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> vec(5, 0);
+    cout << vec[0];
+    cout << vec[1];
+    cout << vec[2];
+    cout << vec[3];
+    cout << vec[4];
+}
