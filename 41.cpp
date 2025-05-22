65ff57848219ecea4574ab5b54e8b71324b5f08b
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-05-22 11:38:00 +0530

    Add files via upload
    Signed-off-by: Tithi Saha <127840793+mynametithi@users.noreply.github.com>

---
+++ 41.cpp
@@ -0,0 +1,12 @@
+// pointers:
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10;
+    int *ptr = &a;
+    cout << ptr << endl;
+    cout << &a << endl;
+    cout << &ptr << endl;
+    return 0;
+}
