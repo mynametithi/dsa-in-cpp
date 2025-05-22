064aafb7c43d9c5c11c0124a5c32a62a1910a7a1
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-05-22 12:26:03 +0530

    Add files via upload
    Signed-off-by: Tithi Saha <127840793+mynametithi@users.noreply.github.com>

--- 42.cpp
+++ 42.cpp
@@ -1,6 +1,14 @@
+//Pointer to Pointer:
+
 #include <iostream>
 using namespace std;
 int main()
 {
+    int a = 10;
+    int *ptr = &a;
+    int **ptr2 = &ptr;
+    cout << &ptr << endl;
+    cout << ptr << endl;
+    cout << ptr2 << endl;
     return 0;
 }
