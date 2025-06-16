44511c309f9f844c86cdbf2f27d1fa8cd680c8b0
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-06-16 23:00:14 +0530

    Add files via upload
    Signed-off-by: Tithi Saha <127840793+mynametithi@users.noreply.github.com>

---
+++ 47.cpp
@@ -0,0 +1,37 @@
+// Binary Search 2nd:
+//  Binary Search:
+#include <iostream>
+#include <vector>
+using namespace std;
+int binarySearch(vector<int> arr, int tar) // iteritive code
+{
+    int st = 0, end = arr.size() - 1;
+    while (st <= end)
+    {
+        int mid = (st + end) / 2;
+        if (tar > arr[mid])
+        {
+            st = mid + 1;
+        }
+        else if (tar < arr[mid])
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            return mid;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    // vector<int> arr1 = {-1, 0, 3, 4, 5, 9, 12};
+    // int tar1 = 40;
+    // cout << binarySearch(arr1, tar1);
+    vector<int> arr2 = {-1, 0, 3, 5, 9, 12};
+    int tar2 = 0;
+    cout << binarySearch(arr2, tar2);
+    return 0;
+}
