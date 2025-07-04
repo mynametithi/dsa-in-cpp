25d781a2c947fe4b522bd133becbcbc9eb4f5af3
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-07-04 23:15:43 +0530

    Add files via upload
    Signed-off-by: Tithi Saha <127840793+mynametithi@users.noreply.github.com>

--- 47.cpp
+++ 47.cpp
@@ -8,7 +8,7 @@ int binarySearch(vector<int> arr, int tar) // iteritive code
     int st = 0, end = arr.size() - 1;
     while (st <= end)
     {
-        int mid = (st + end) / 2;
+        int mid = st + (end - st) / 2; // same as (st+end)/2    
         if (tar > arr[mid])
         {
             st = mid + 1;
