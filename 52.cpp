57563ff388273480627a645538e6ffe0cadad5b8
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-07-22 21:56:16 +0530

    Add files via upload
    Signed-off-by: Tithi Saha <127840793+mynametithi@users.noreply.github.com>

---
+++ 52.cpp
@@ -0,0 +1,13 @@
+// sum of number 1 to n:
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 10;
+    for (int i = 1; i <= n; i = i + 2)
+    {
+        cout << i << endl;
+    }
+    cout << endl;
+    return 0;
+}
