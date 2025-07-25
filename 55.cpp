0c1e7808dbe614a3d7ff045121e34d1301407ccd
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-07-25 22:11:32 +0530

    Add files via upload
    Signed-off-by: Tithi Saha <127840793+mynametithi@users.noreply.github.com>

---
+++ 55.cpp
@@ -0,0 +1,18 @@
+// loop:
+//  loop:
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "limit of your number:";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << " " << "*";
+        }
+        cout << endl;
+    }
+}
