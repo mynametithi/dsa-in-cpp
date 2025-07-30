764df583fe8d1c16f1d70de1b176b2ed817c3925
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-07-30 23:57:35 +0530

    Add files via upload
    Signed-off-by: Tithi Saha <127840793+mynametithi@users.noreply.github.com>

---
+++ 19.cpp
@@ -0,0 +1,12 @@
+// Practice
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << sizeof(int) << endl;
+    cout << sizeof(short int) << endl;
+    cout << sizeof(long long int) << endl;
+    // cout << sizeof(int) << endl;
+    // cout << sizeof(int) << endl;
+}
