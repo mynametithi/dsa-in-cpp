40e0d869d6dc49c79977beed8c39d6ed385d41b7
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-08-03 23:23:11 +0530

    Add files via upload
    Signed-off-by: Tithi Saha <127840793+mynametithi@users.noreply.github.com>

---
+++ 65.cpp
@@ -0,0 +1,18 @@
+// triangle star print using loop:
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter your limit:";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << (i + 1) << " ";
+        }
+        cout << endl;
+    }
+}
