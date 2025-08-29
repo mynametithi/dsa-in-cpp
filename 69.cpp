2e1bffb026a86c0689141051472fa85f84579eab
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-08-29 07:28:21 +0530

    Add files via upload
    Signed-off-by: Tithi Saha <127840793+mynametithi@users.noreply.github.com>

---
+++ 69.cpp
@@ -0,0 +1,18 @@
+// function:
+// calculate factorial using function:
+#include <iostream>
+using namespace std;
+int factN(int n)
+{
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
+int main()
+{
+    cout << factN(4) << endl;
+    cout << factN(5) << endl;
+}
