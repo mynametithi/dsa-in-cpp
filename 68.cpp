66d6bdfc75a5c8e4e681757b566886971af0fcf8
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2025-08-29 07:28:05 +0530

    Add files via upload
    Signed-off-by: Tithi Saha <127840793+mynametithi@users.noreply.github.com>

--- 68.cpp
+++ 68.cpp
@@ -1,8 +1,17 @@
-// pattern:
+// Funtions:
 #include <iostream>
 using namespace std;
+int sumN(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
 int main()
 {
-    int n;
-    in
+    cout << sumN(5) << endl;
+    cout << sumN(4) << endl;
 }
